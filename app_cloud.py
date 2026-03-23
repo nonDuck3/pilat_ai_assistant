dd75d0f1a7db6907cee67e10a62f4f9bc781ccc5
nonDuck3 <sulxie32@gmail.com>
2026-03-23 23:03:06 +1100

    Update app_cloud.py

--- app_cloud.py
+++ app_cloud.py
@@ -4,16 +4,11 @@ from src.text_retrieval_search import query_vector_store
 import streamlit as st
 from dotenv import load_dotenv
 import os
-import argparse
 from openai import OpenAI
 
 load_dotenv()
 
 prompt_file = "../prompts/system_prompt.md"
-
-parser = argparse.ArgumentParser(description="RAG-enabled LLM conversational agent with Streamlit UI.")
-parser.add_argument("collection_name", type=str, help="The name of the collection to retrieve vector embeddings")
-args = parser.parse_args()
 
 with open(prompt_file, 'r', encoding='utf-8') as file:
     base_system_prompt = file.read()
@@ -31,7 +26,7 @@ google_client = OpenAI(
 )
 
 client = get_vector_store_cloud_client(tenant, db, chroma_api_key)
-collection = client.get_collection(name=args.collection_name)
+collection = client.get_collection(name="pilates_guide_collection")
 
 st.set_page_config(
     page_title="Pilat.ai",
